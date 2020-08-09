630817a7c8176ec35b314c753747284039168fcc
Yarden125 <yardenmor125@gmail.com>
2020-08-09 13:41:27 +0300

    secret

--- middlewares/verify-logged-in.js
+++ middlewares/verify-logged-in.js
@@ -1,24 +1,25 @@
 const jwt = require("jsonwebtoken");
+const secret = process.env.SECRET || "Chuck-Norris";
 
 // Verify logged in with token:
-function verifyLoggedIn(request, response,next){
-    const token = request.headers.authorization;
+function verifyLoggedIn(request, response, next) {
+  const token = request.headers.authorization;
 
-    // If token doesn't exist:
-    if(!token){
-        response.status(401).send("You are not logged-in");
-        return;
+  // If token doesn't exist:
+  if (!token) {
+    response.status(401).send("You are not logged-in");
+    return;
+  }
+
+  //if token illegal or expired:
+  jwt.verify(token, secret, (err) => {
+    if (err) {
+      response.status(401).send("You are not logged-in");
+      return;
     }
-
-    //if token illegal or expired:
-    jwt.verify(token, "Chuck-Norris", err =>{
-        if(err){
-            response.status(401).send("You are not logged-in");
-            return;
-        }
-        // if user is logged in:
-        next();
-    });
+    // if user is logged in:
+    next();
+  });
 }
 
 module.exports = verifyLoggedIn;
